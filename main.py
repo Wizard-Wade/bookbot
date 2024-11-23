715d61e5a12ecf7150ee744488d1af1f786eccd8
Wizard-Wade <wade.vollink@gmail.com>
2024-11-22 21:53:51 -0700

    add get letters function to create dictionary of letter count

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ path_to_file = "books/frankenstein.txt"
 def main():
     text = get_book_text(path_to_file)
     num_words = get_num_words(text)
-    print(num_words)
+    letter_count = get_letters(text)
+    print(letter_count)
     
 def get_num_words(text):
     words = text.split()
     return len(words)
 
+def get_letters(text):
+    lower_text = text.lower()
+    alphabet_dict = {}
+    for char in lower_text:
+        if char in alphabet_dict:
+            alphabet_dict[char] +=1
+        else:
+            alphabet_dict[char] = 1
+    return alphabet_dict
+            
+    
 def get_book_text(path):
     with open(path) as f:
         return f.read()
