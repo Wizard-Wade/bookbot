0471a69abd9c763adc499d05b5c34e07253302a3
Wizard-Wade <wade.vollink@gmail.com>
2024-11-23 22:34:24 -0700

    sort dictionary by letter usage and remove non alphabetical characters

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ def main():
     text = get_book_text(path_to_file)
     num_words = get_num_words(text)
     letter_count = get_letters(text)
-    print(letter_count)
+    sort = sort_letters(letter_count)
+    
+    
+    print("--- Begin report of books/frankenstein.txt ---")
+    print (num_words)
+    for key, value in sort.items():
+        print(f"The '{key}' character was found {value} times")
+    print("--- End report ---")
+    
+def sort_letters(dict_letters):
+    sort = dict(sorted(dict_letters.items(),key= lambda item : item[1], reverse=True))
+    return sort
     
 def get_num_words(text):
     words = text.split()
@@ -14,6 +25,8 @@ def get_letters(text):
     lower_text = text.lower()
     alphabet_dict = {}
     for char in lower_text:
+        if not char.isalpha():
+            continue
         if char in alphabet_dict:
             alphabet_dict[char] +=1
         else:
